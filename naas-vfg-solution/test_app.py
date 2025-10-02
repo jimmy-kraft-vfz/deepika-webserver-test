c28845fa5be9e4ae7d83dea5848e387a0a174580
Deepika Gupta <31510092+dg06@users.noreply.github.com>
2025-10-02 16:50:33 +0200

    Update test_app.py

--- naas-vfg-solution/test_app.py
+++ naas-vfg-solution/test_app.py
@@ -3,10 +3,9 @@ from app import app
 import json
 
 def test_root_returns_200():
-    client = app.test_client()
-    resp = client.get("/")
-    assert resp.status_code == 200
-    assert b"Hello DevOps NaaS" in resp.data
+    with app.test_client() as client:
+    response = client.get("/")
+    assert response.status_code == 200
 
 def test_health():
     client = app.test_client()
